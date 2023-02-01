7c005017a5aab78e1032c72eadba640662cb6670
herophil <philip.herold.01@gmail.com>
2023-02-01 07:26:11 +0100

    remove temp files

--- temp.java
+++ temp.java
@@ -2,12 +2,30 @@ public class temp {
 
     public static void main(String[] args) {
         
-        int[] array = new int[]{1,2,3,4,5,6,7,8,9,10};
+        ArrayList<int> array = new ArrayList<int>{1,2,3,4,5,6,7,8,9,10};
         // 
 
         for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
+            int value = array[index];
+
+            if(index > 0) {
+                System.out.println("Ich bin nicht der erste");
+            }
+
+
+
+            System.out.println(value);
         }
+
+
+        for (int value : array) {
+
+            System.out.println(value);
+
+
+        }
+
+
 
 
 
